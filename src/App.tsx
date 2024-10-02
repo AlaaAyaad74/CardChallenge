87d2d07b551ee9b1adb59e4a5c196e2b8a10f681
AlaaAyaad74 <alaaayad1@outlook.come>
2024-10-02 12:04:30 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import Card from "./components/cards/card";
 
 function App() {
   const [count, setCounter] = useState(0);
+  //  'useMemo Hook to reduce the rerender times only when the 'arr' value changes
+  const arr: number[] = useMemo(() => [1, 0, 1, 1, 1, 0], []);
+  // select all Elemnts that have class "active"
+  const elements = document.querySelectorAll(
+    ".active"
+  ) as NodeListOf<HTMLElement>;
+  console.log(elements);
   useEffect(() => {
-    if (count === 7) {
-      const elements = document.querySelectorAll(".active");
-
-      elements.forEach((item) => {
-        const timer = Number(item.getAttribute("timer"));
-
-        setTimeout(() => {
-          item.classList.remove("active");
-        }, timer * 800);
-      });
-      setCounter(0);
+    if (extractOns(arr) === elements.length) {
+      setTime(elements);
     }
-  }, [count]);
+  }, [elements, arr]);
+  // This function get length of all buttons that have opacity 1
+  const extractOns = (arr: number[]) => {
+    return arr.filter((item) => (item ? item : "")).length;
+  };
+  // function that setTimeOut to remove all classes actives Forward to Arrange You Added It
+  const setTime = (elements: NodeListOf<HTMLElement>) => {
+    elements.forEach((item) => {
+      const timer = Number(item.getAttribute("timer"));
+      setTimeout(() => {
+        item.classList.remove("active");
+      }, timer * 800);
+    });
+  };
+
+  // function that set Active class into buttons
   const activeFun = (e: React.MouseEvent<HTMLButtonElement>): void => {
-    e.currentTarget.classList.add("active");
-    e.currentTarget.setAttribute("timer", `${count}`);
-    setCounter((prevCount) => prevCount + 1);
+    if (+e.currentTarget.style.opacity !== 0) {
+      e.currentTarget.classList.add("active");
+      e.currentTarget.setAttribute("timer", `${count}`);
+      setCounter((prevCount) => prevCount + 1);
+    }
   };
   return (
     <div className="flex flex-wrap gap-5">
-      {Array.from({ length: 7 }, (_, index: number) => (
+      {arr.map((item, index: number) => (
         <Card
           key={index}
+          opacity={item}
           onClick={(e: React.MouseEvent<HTMLButtonElement>) => activeFun(e)}
         />
       ))}
--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 interface Props {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  opacity: number;
 }
-function Card({ onClick }: Props) {
+function Card({ onClick, opacity }: Props) {
   return (
     <button
       className="w-[150px] h-[200px] rounded-md border-4 m-auto"
+      style={{ opacity }}
       onClick={onClick}
     >
       card
